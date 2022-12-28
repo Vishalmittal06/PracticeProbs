0c2541acad2eb8528fdba39f3998b35c7a39b02e
back_to_code <mittal.vishal06@gmail.com>
2022-12-28 23:07:16 +0530

    Main.java Default Program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int[] arr = {1,2,7,8,19,38,8,9,10,2,7};
+        SelectionSort sorting = new SelectionSort();
+        sorting.sort(arr);
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
     }
 }
