48adfb0c5efc98a30d5ac2ddd66f84ad3fe47186
back_to_code <mittal.vishal06@gmail.com>
2022-12-28 22:53:03 +0530

    Main.java Default Program

---
+++ src/SelectionSort.java
@@ -0,0 +1,20 @@
+public class SelectionSort {
+    public void sort(int[] arr){
+        int currentIndex=0;
+        while(currentIndex<arr.length){
+            int minIndex= currentIndex;
+            for(int index=currentIndex;index<arr.length;index++){
+                if(arr[index]<arr[minIndex])
+                    minIndex = index;
+            }
+            swap(arr,minIndex,currentIndex);
+            currentIndex++;
+        }
+    }
+
+    private void swap(int[] arr,int minIndex,int currentIndex){
+        int temp = arr[currentIndex];
+        arr[currentIndex] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
